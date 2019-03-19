bab61a4ced03d9668f783b629fe126e9bb44f087
Gilnei Junior <gilnei.junior123@gmail.com>
2019-03-19 00:04:53 -0300

    Tela de Menu, tiro de flecha, interação de player com inimigo

---
+++ Assets/Scripts/Flecha.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Flecha : MonoBehaviour
+{
+    public float alcance = 3;
+    public float velocidade = 0.1f;
+
+    private float startPosY;
+     
+    private float heightCamera;
+
+    public GameObject Inimigo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosY = transform.position.y;
+        heightCamera = Camera.main.orthographicSize;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (transform.position.y >= heightCamera)
+        {
+            Destroy(gameObject);
+        }
+
+        transform.position = new Vector2(transform.position.x, transform.position.y + velocidade); 
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Inimigo"))
+        {
+            Destroy(gameObject);
+            Destroy(col.gameObject);
+
+            Instantiate(Inimigo, new Vector2(-5, 2), Quaternion.identity);
+
+        }
+    }
+}
---
+++ Assets/Scripts/Jogador.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Jogador : MonoBehaviour
+{
+
+    public float velocidade = 0.1f;
+    public float forcaPulo = 17f;
+    public Rigidbody2D rb;
+
+    public GameObject Flecha;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow)) MoverDireita();
+        if (Input.GetKey(KeyCode.LeftArrow)) MoverEsquerda();   
+        if (Input.GetKeyDown(KeyCode.UpArrow)) Pular();
+        if (Input.GetKeyDown(KeyCode.Space)) Atirar();
+    }
+
+    public void MoverDireita()
+    {
+        transform.position = new Vector2(transform.position.x + velocidade, transform.position.y);
+    }
+
+    public void MoverEsquerda()
+    {
+        transform.position = new Vector2(transform.position.x - velocidade, transform.position.y);
+    }
+    
+    public void Pular()
+    {
+        rb.AddForce(new Vector2(0, forcaPulo), ForceMode2D.Impulse);
+    }
+
+    public void Atirar()
+    {
+        Instantiate(Flecha, new Vector2(transform.position.x, transform.position.y), Quaternion.identity);
+    }
+
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+        if (col.gameObject.CompareTag("Inimigo"))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+
+}
